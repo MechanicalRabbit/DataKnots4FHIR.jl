b7af24f6a3a28013faefed7f0b8168e271eb5e1a
Clark C. Evans <cce@clarkevans.com>
2020-06-23 18:16:30 -0500

    Determine cardinality by looking at min/max values; don't include elements where the maximum number of elements are 0

--- src/profile.jl
+++ src/profile.jl
@@ -14,29 +14,43 @@ function load_profiles()
     return convert(DataKnot, items)
 end
 
+function determine_cardinality(min::Integer, max::String)
+    if min == 0
+        if max == "1"
+            return Is0to1
+        else
+            return Is0toN
+        end
+    else
+        if max == "1"
+            return Is1to1
+        else
+            return Is1toN
+        end
+    end
+end
+
 Attributes =
   It.snapshot >> Is(Dict) >>
   It.element >> Is(Vector) >> Is(Dict) >>
-  Given(
-    :min => It.min >> convert.(Unsigned, It),
-    :max => It.max >> Is(String),
-    Record(
-      :id => It.id >> Is(String),
-      :path => It.path >> Is(String),
-      :type =>
-        It.type >> Is(Union{Vector, Missing}) >>
-        Is(Vector) >> Is(Dict) >>
-        Record(
-          :code => It.code >> Is(String),
-          :extension => It.extension >> Is(Union{Vector, Missing}) >>
-            Is(Vector) >> Is(Dict) >>
-            Record(
-              :valueUrl => It.valueUrl >> Is(Union{String, Missing}),
-              :url => It.url >> Is(String),
-              :valueBoolean => It.valueBoolean >> Is(Union{Bool, Missing})
-            )
-        )
-    )
+  Filter(It.max .!= "0") >>
+  Record(
+    :id => It.id >> Is(String),
+    :path => It.path >> Is(String),
+    :card => determine_cardinality.(It.min, It.max),
+    :type =>
+      It.type >> Is(Union{Vector, Missing}) >>
+      Is(Vector) >> Is(Dict) >>
+      Record(
+        :code => It.code >> Is(String),
+        :extension => It.extension >> Is(Union{Vector, Missing}) >>
+          Is(Vector) >> Is(Dict) >>
+          Record(
+            :valueUrl => It.valueUrl >> Is(Union{String, Missing}),
+            :url => It.url >> Is(String),
+            :valueBoolean => It.valueBoolean >> Is(Union{Bool, Missing})
+          )
+      )
   )
 
 UnpackProfile =
@@ -55,7 +69,6 @@ UnpackProfile =
 
 function verify_assumptions(knot)
     @assert(0 == length(get(knot[Filter(It.type .!== It.first.path)])))
-
 end
 
 function testfhir()
