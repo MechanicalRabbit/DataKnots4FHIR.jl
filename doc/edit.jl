b420cf41370c043aa5bed7fb4c0692a7942508ee
Clark C. Evans <cce@clarkevans.com>
2020-07-07 13:50:52 -0500

    Updating documentation bootstrap to incude Revise and also set the REPL terminal size to 72 columns, 2/3 rows.

--- doc/edit.jl
+++ doc/edit.jl
@@ -1,5 +1,22 @@
 using NarrativeTest
+
 make() = withenv("LINES"=>11, "COLUMNS"=>72) do
+             Base.active_repl.options.
+                iocontext[:displaysize] = (convert(Integer, 
+                   trunc(displaysize(Base.stdout)[1] * 2/3)), 72)
              runtests(joinpath("doc/src", ENV["DOCFILE"]), mod=Main)
          end
-make()
+
+atreplinit() do repl
+    try
+        @eval using BenchmarkTools: @btime
+        @eval using Revise
+        @eval make()
+        @eval using DataKnots
+        @eval using DataKnots4FHIR
+        @async Revise.wait_steal_repl_backend()
+    catch
+        @warn "startup error?"
+    end
+end
+
